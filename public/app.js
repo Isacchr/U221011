1796d7ec6218ff4ef83cdda9aed1d9da859c6a02
isacchr <isac.christiansson@gmail.com>
2022-10-11 12:06:18 +0200

    input added and can get output in console.log

--- public/app.js
+++ public/app.js
@@ -1,1 +1,31 @@
-console.log('hi hi')
+var contacts = [];
+
+build = () => {
+
+    /*const XHR = new XMLHttpRequest();
+
+    XHR.onload = () => {
+
+
+        
+    }
+
+    XHR.open();
+    XHR.send(); */
+
+}
+
+addContact = () => {
+
+    const name = document.getElementById('name').value;
+    const phonenumber = document.getElementById('phonenumber').value;
+    const c = document.getElementById('email').value;
+
+    var contactOrder = {name: name, phonenumber: phonenumber, email: phonenumber};
+
+    contacts.push(contactOrder);
+
+    console.log(contactOrder);
+
+
+}
--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const bodyParser = require('body-parser');
 
 server.use(express.static('public'));
 server.use(bodyParser.urlencoded({ extended: true}));
+
+server.get('/', (req, res) => {
+
+    fs.readFile('/public/index.html', (err, data) => {
+
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data);
+        return res.end();
+
+    });
+
+    
+});
